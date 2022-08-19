14b3b7def481862e8ad819dcd261398abfbfa6e3
chc <99154196+ch77ancreant@users.noreply.github.com>
2022-08-19 21:33:18 +0800

    update files

--- 1/main_b.m
+++ 1/main_b.m
@@ -9,6 +9,9 @@ p = [0.5;...
     -0.4;...
     -0.2];
 
+% sd2 = E[|d(n)|^2]
+sd2 = 1;
+
 Re_w0 = linspace(-5, 5, 101);
 J = zeros(1, 101);
 for ii = 1:101
@@ -17,13 +20,13 @@ for ii = 1:101
 end
 
 Jmin = min(J);
-Jmin_x = find(J == Jmin);
+min_x = find(J == Jmin);
 
 % plot
-txt = ['optimal point is at ' 10 'Re\{w0\} = ' num2str(Re_w0(Jmin_x)) 10 'and associated Jmin = ' num2str(Jmin)];
+txt = ['optimal point is at ' 10 'Re\{w0\} = ' num2str(Re_w0(min_x)) 10 'and Jmin is ' num2str(Jmin)];
 text(-1, 10, txt, 'color', 'red', 'Fontsize', 10)
 hold on
-plot(Re_w0(Jmin_x), Jmin, '*', 'color', 'r', 'markersize',10);
+plot(Re_w0(min_x), Jmin, '*', 'color', 'r', 'markersize',10);
 plot(Re_w0, J, 'color', 'blue');
 xlabel('Re\{w0\}');
 ylabel('MSE');
