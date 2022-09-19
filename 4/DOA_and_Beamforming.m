b1ff0f9f2bea7b38829f8e7b5285ed1d1b4162d3
chc <99154196+ch77ancreant@users.noreply.github.com>
2022-09-19 14:01:54 +0800

    update files

--- 4/DOA_and_Beamforming.m
+++ 4/DOA_and_Beamforming.m
@@ -1,20 +1,20 @@
-
+clear; close all;
 
 load('matX.mat');
 
 % Covariance matrix estimation
-R_hat = (matX*matX')/1000;
+R_hat = (matX * matX') / 1000;
+
 % Discrete grid theta -----------
 theta = -90:0.01:90;
-
 
 %% DOA estimation
 
 % Scan all the theta and compute the spetrum
 P_theta = zeros(1, length(theta));
-for jj = 1:length(theta)
-    a = exp(1i*[0:10]*pi*sind(theta(jj))).';
-    P_theta(jj) = 1/(a'*(R_hat\a));
+for j = 1:length(theta)
+    a = exp(1i*[0:10]*pi*sind(theta(j))).';
+    P_theta(j) = 1/(a'*(R_hat\a));
 end
 
 % Find the peaks on the sepctrum
@@ -22,67 +22,67 @@ end
 locs = theta(locs);
 
 % plot
-figure(1);
-plot(theta,abs(P_theta));
-title('MVDR spectrum');
-xlabel('$\theta$ (rad)','interpreter','latex');
-set(gca,'xtick',[-90 -45 0 45 90]);
-set(gca,'xticklabel',{'-\pi/2','-\pi/4','0','\pi/4','\pi/2'});
-ylabel('$P_{MVDR}(\theta)$','interpreter','latex');
-axis([-90 90 0 8]);
+figure
+plot(theta, abs(P_theta))
+title('MVDR spectrum')
+xlabel('$\theta$ (rad)','interpreter','latex')
+set(gca,'xtick',[-90 -45 0 45 90])
+set(gca,'xticklabel',{'-\pi/2','-\pi/4','0','\pi/4','\pi/2'})
+ylabel('$P_{MVDR}(\theta)$','interpreter','latex')
+axis([-90 90 0 8])
 grid on
 
 
 %% Beampattern
 
-B_Uniform = zeros(1,length(theta));
-B_AS = zeros(1,length(theta));
-B_MVDR = zeros(1,length(theta));
+B_Uniform = zeros(1, length(theta));
+B_AS = zeros(1, length(theta));
+B_MVDR = zeros(1, length(theta));
 
 % a(theta_s) ----------------------------
 a_s = exp(1i*[0:10]*pi*sind(3.25)).';
 
 % -----------------------------------------
 % The weights of different beamformer
-w_Uniform = 1/11*ones(11,1);
-w_AS = 1/11*a_s;
-w_MVDR = (R_hat\a_s)/(a_s'*(R_hat\a_s));
+w_Uniform = 1/11 * ones(11,1);
+w_AS = 1/11 * a_s;
+w_MVDR = (R_hat\a_s) / (a_s'*(R_hat\a_s));
 
 % B_theta(theta) = w' x a(theta)
 % Uniform weights, w = 1/N
-for ii = 1:length(theta)
-     B_Uniform(ii) =  sum(exp(1i*pi*sind(theta(ii))*[0:10]));
+for i = 1:length(theta)
+     B_Uniform(i) =  sum(exp(1i*pi*sind(theta(i))*[0:10]));
 end
  
 % Array steering with source DOA
-for ii = 1:length(theta)
-     B_AS(ii) =  sum(exp(1i*pi*(sind(theta(ii)) - sind(3.25))*[0:10]));
+for i = 1:length(theta)
+     B_AS(i) =  sum(exp(1i*pi*(sind(theta(i)) - sind(3.25))*[0:10]));
 end
   
 % MVDR beamformer with source DOA
-for ii = 1:length(theta)
-    a = exp(1i*[0:10]*pi*sind(theta(ii))).';
-    B_MVDR(ii) = w_MVDR'*a;
+for i = 1:length(theta)
+    a = exp(1i*[0:10]*pi*sind(theta(i))).';
+    B_MVDR(i) = w_MVDR'*a;
 end
 
-Beam_ULA = 1/11*abs(B_Uniform);
-Beam_AS = 1/11*abs(B_AS);
+Beam_ULA = 1/11 * abs(B_Uniform);
+Beam_AS = 1/11 * abs(B_AS);
 Beam_MVDR = abs(B_MVDR);
 
 % plot 
-figure(2);
-plot(theta,20*log10(Beam_ULA),'linewidth',2);
+figure
+plot(theta, 20*log10(Beam_ULA),'linewidth',2)
 hold on
-plot(theta,20*log10(Beam_AS),'linewidth',2);
-plot(theta,20*log10(Beam_MVDR),'linewidth',2);
-title('Beampattern');
-xlabel('$\theta$ (rad)','interpreter','latex');
-ylabel('$|B_{\theta}(\theta)|$ (dB)','interpreter','latex');
-legend('Uniform weights','Array steering','MVDR');
-axis([-90 90 -80 20]);
-set(gca,'ytick',[-80 -60 -40 -20 0 20]);
-set(gca,'xtick',[-90 -45 0 45 90]);
-set(gca,'xticklabel',{'-\pi/2','-\pi/4','0','\pi/4','\pi/2'});
+plot(theta, 20*log10(Beam_AS),'linewidth',2)
+plot(theta, 20*log10(Beam_MVDR),'linewidth',2)
+title('Beampattern')
+xlabel('$\theta$ (rad)','interpreter','latex')
+ylabel('$|B_{\theta}(\theta)|$ (dB)','interpreter','latex')
+legend('Uniform weights','Array steering','MVDR')
+axis([-90 90 -80 20])
+set(gca,'ytick',[-80 -60 -40 -20 0 20])
+set(gca,'xtick',[-90 -45 0 45 90])
+set(gca,'xticklabel',{'-\pi/2','-\pi/4','0','\pi/4','\pi/2'})
 grid on
 
 
@@ -94,39 +94,39 @@ y_MVDR = zeros(1, 1000);
 
 % Uniform weight
 for k = 1:1000
-    y_Uniform(k) = w_Uniform'*matX(:,k);
+    y_Uniform(k) = w_Uniform' * matX(:,k);
 end
 
 % Array steering
 for k = 1:1000
-    y_AS(k) = w_AS'*matX(:,k);
+    y_AS(k) = w_AS' * matX(:,k);
 end
 
 % MVDR beamformer
 for k = 1:1000
-    y_MVDR(k) = w_MVDR'*matX(:,k);
+    y_MVDR(k) = w_MVDR' * matX(:,k);
 end
 
 % plot
-figure(3);
-subplot(2,1,1);
-plot(1:1000, real(y_Uniform));
+figure
+subplot(2,1,1)
+plot(1:1000, real(y_Uniform))
 hold on
-plot(1:1000, real(y_AS));
-plot(1:1000, real(y_MVDR));
-title('The output of beamformer (real parts)');
-xlabel('t','interpreter','latex');
-ylabel('$Re\{y(t)\}$','interpreter','latex');
-legend('Uniform weights','Array steering','MVDR');
+plot(1:1000, real(y_AS))
+plot(1:1000, real(y_MVDR))
+title('The output of beamformer (real parts)')
+xlabel('t','interpreter','latex')
+ylabel('$Re\{y(t)\}$','interpreter','latex')
+legend('Uniform weights','Array steering','MVDR')
 
-subplot(2,1,2);
-plot(1:1000, imag(y_Uniform));
+subplot(2,1,2)
+plot(1:1000, imag(y_Uniform))
 hold on
-plot(1:1000, imag(y_AS));
-plot(1:1000, imag(y_MVDR));
-title('The output of beamformer (imaginary parts)');
-xlabel('t','interpreter','latex');
-ylabel('$Im\{y(t)\}$','interpreter','latex');
-legend('Uniform weights','Array steering','MVDR');
+plot(1:1000, imag(y_AS))
+plot(1:1000, imag(y_MVDR))
+title('The output of beamformer (imaginary parts)')
+xlabel('t','interpreter','latex')
+ylabel('$Im\{y(t)\}$','interpreter','latex')
+legend('Uniform weights','Array steering','MVDR')
 
 
--- 4/LCMV_Beampattern.m
+++ 4/LCMV_Beampattern.m
@@ -1,9 +1,10 @@
-
+clear; close all;
 
 load('matX.mat');
 
 % Covariance matrix estimation
-R_hat = (matX*matX')/1000;
+R_hat = (matX*matX') / 1000;
+
 % Discrete grid theta -----------
 theta = -90:0.01:90;
 
@@ -14,21 +15,21 @@ a_s = exp(1i*[0:10]*pi*sind(3.25)).';
 % a(theta_i) ----------------------------
 a_i = exp(1i*[0:10]*pi*sind(18.57)).';
 
-C = [a_s , a_i];
+C = [a_s, a_i];
 g = [1; 10^(-5)];
 w_LCMV = R_hat\C*((C'*(R_hat\C))\g);
 
 % LCMV beamformer
-for ii = 1:length(theta)
-    a = exp(1i*[0:10]*pi*sind(theta(ii))).';
-    B_LCMV(ii) = w_LCMV'*a;
+for i = 1:length(theta)
+    a = exp(1i*[0:10]*pi*sind(theta(i))).';
+    B_LCMV(i) = w_LCMV'*a;
 end
 
 % plot
 plot(theta, 20*log10(abs(B_LCMV)), 'linewidth', 2);
 hold on
-line([3.25 3.25],[-80 20],'LineStyle',':','LineWidth',1.3,'Color',[0.8500 0.3250 0.0980])
-line([18.57 18.57],[-80 20],'LineStyle',':','LineWidth',1.3,'Color',[0.4940 0.1840 0.5560])
+line([3.25 3.25], [-80 20],'LineStyle',':','LineWidth',1.3,'Color',[0.8500 0.3250 0.0980])
+line([18.57 18.57], [-80 20],'LineStyle',':','LineWidth',1.3,'Color',[0.4940 0.1840 0.5560])
 legend('beampattern','signal direction','interference direction','Location','southwest')
 title('LCMV Beampattern');
 xlabel('$\theta$ (rad)','interpreter','latex');
@@ -38,6 +39,5 @@ set(gca,'ytick',[-80 -60 -40 -20 0 20]);
 set(gca,'xtick',[-90 -45 0 45 90]);
 set(gca,'xticklabel',{'-\pi/2','-\pi/4','0','\pi/4','\pi/2'});
 grid on
-
 
 
